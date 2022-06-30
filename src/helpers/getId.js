f9a358172b363327794e32630e6e608d29ed1069
Arath <arath_19_4@hotmail.com>
2022-06-30 11:56:51 -0500

    Se creo el count auntomatico de IDs

--- src/helpers/getId.js
+++ src/helpers/getId.js
@@ -1,23 +1,40 @@
 const getID = async (model, ocid = false) => {
   const cont = await model.count(); //1
   let numID = 0;
-  numID = cont+1;
-  console.log(numID);
-  if (ocid){
-     return setOCID(numID);
+  numID = cont + 1;
+  if (ocid) {
+    return setOCID(numID);
   }
   return numID;
 };
 
 const setOCID = (id) => {
-    ocid = process.env.OCID
-    ocid += `000-0000${id}`
-    
-    console.log(typeof ocid);
-    return ocid
+  ocid = process.env.OCID;
+  idi = zfill(id, 5);
+  ocid += `000-${idi}`;
 
+  console.log(ocid);
+  return ocid;
+};
+
+function zfill(number, width) {
+  var numberOutput = Math.abs(number); /* Valor absoluto del número */
+  var length = number.toString().length; /* Largo del número */
+  var zero = "0"; /* String de cero */
+
+  if (width <= length) {
+    if (number < 0) {
+      return "-" + numberOutput.toString();
+    } else {
+      return numberOutput.toString();
+    }
+  } else {
+    if (number < 0) {
+      return "-" + zero.repeat(width - length) + numberOutput.toString();
+    } else {
+      return zero.repeat(width - length) + numberOutput.toString();
+    }
+  }
 }
-
-
 
 module.exports = getID;
