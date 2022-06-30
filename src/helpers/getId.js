1c7e61a89d0ada882667c7b26f1967993d80399e
HotaruV <faotqm@hotmail.com>
2022-06-30 10:40:49 -0500

    funcion ocid

--- src/helpers/getId.js
+++ src/helpers/getId.js
@@ -1,9 +1,24 @@
-const getID = async (model) => {
+const getID = async (model, ocid = false) => {
   const cont = await model.count(); //1
   let numID = 0;
   numID = cont+1;
+
   console.log(numID);
+  if (ocid){
+     return setOCID(numID);
+
+  }
+
   return numID;
 };
 
-module.exports = getID;
+const setOCID = (id) => {
+    ocid = process.env.OCID
+    ocid = ocid + id
+    return ocid
+
+}
+
+
+
+module.exports = {getID};
