faf3ab9e8648c09b0a1e864a91a92974e4b8c8e5
Arath <arath_19_4@hotmail.com>
2022-07-04 18:13:26 -0500

    se cambio licitacion por release y se acomodaron sus campos

---
+++ src/Controllers/ReleaseController.js
@@ -0,0 +1,48 @@
+const { response } = require("express");
+const { JWTgenerate } = require("../helpers/jwt");
+const { v4: uuidv4 } = require("uuid");
+const licitacion = require("../models/licitacion");
+const { populate } = require("../models/usuario");
+const getID = require("../helpers/getId");
+
+const ReleaseController = {
+  licitacionCreate: async (req, res = response) => {
+    const lic = new licitacion(req.body);
+    await lic.save();
+    return res.status(400).json({
+      ok: true,
+      lic,
+    });
+  },
+    
+  licitacionShow: async (req, res = response) => {
+    const id = req.params.id;
+    let ID = getID(licitacion);
+    const lic = await licitacion
+      .findById(id)
+      .populate("parties",'-__v')
+      .populate("buyer",'-__v')
+      .populate("tender",'-__v');
+    if (!lic) {
+      return res.status(404).json({
+        ok: false,
+        msg: "No existe esta partie",
+      });
+    }
+    res.status(200).json({
+        "release": {
+          "ocid": lic.ocid,
+          "id":lic.id,
+          "date":lic.date,
+          "language":lic.language,
+          "tag":lic.tag,
+          "initiationType":lic.initiationType,
+          "parties":lic.parties,
+          "buyer":lic.buyer,
+          "tender":lic.tender
+        }
+    });
+  }
+};
+
+module.exports = ReleaseController;
