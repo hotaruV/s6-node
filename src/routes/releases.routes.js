cbca079d3da019a379f38007737d4902b88c9863
Arath <arath_19_4@hotmail.com>
2022-07-04 18:12:27 -0500

    se cambio licitacion por release y se acomodaron sus campos

--- src/routes/releases.routes.js
+++ src/routes/releases.routes.js
@@ -1,11 +1,11 @@
 const { Router } = require('express');
 const { validarJWT } = require('../middlewares/validar-jwt');
-const LicitacionController = require('../Controllers/LicitacionController');
+const ReleaseController = require('../Controllers/ReleaseController');
 const E_fileUpload = require('express-fileupload');
 const route = Router();
 route.use(E_fileUpload())
 
-route.post("/licitacion", [validarJWT], LicitacionController.licitacionCreate);
-route.get("/licitacion/:id",  LicitacionController.licitacionShow);
+route.post("/licitacion", [validarJWT], ReleaseController.licitacionCreate);
+route.get("/licitacion/:id",  ReleaseController.licitacionShow);
 
 module.exports = route;
